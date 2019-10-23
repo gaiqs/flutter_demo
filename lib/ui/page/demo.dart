6799347cd4a759d2ec2c6762b195b663a5273b01
gaiqs <gaiqisong@auto-link.com.cn>
2019-10-23 17:35:00 +0800

    测试

--- lib/ui/page/demo.dart
+++ lib/ui/page/demo.dart
@@ -10,11 +10,34 @@ class Demo extends StatefulWidget {
 class _DemoState extends State<Demo> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'demo',
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-      ),
+    return ListView(
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[900],
+          child: Center(child: Text("原生交互")),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[800],
+          child: const Center(child: Text('测试数据')),
+        ),
+        Container(
+          constraints: new BoxConstraints.expand(
+            height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+          ),
+          decoration: new BoxDecoration(
+            border: new Border.all(width: 2.0, color: Colors.red),
+            color: Colors.grey,
+            borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+            image: new DecorationImage(
+              image: new NetworkImage(
+                  'http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
+              centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
